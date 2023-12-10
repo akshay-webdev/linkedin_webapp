119a358a7fa753cfdb30f5a24b7828be4d447982
Akshay Wakle <108985906+akshay-webdev@users.noreply.github.com>
2023-12-10 22:32:22 +0530

    mui installed

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from "./components/Header"
+import './CSS/Header.css'
+function App() {
+  return (
+   <>
+  
+    <Header/>
+      {/* <section class="hero">
+        <div class="left-section">
+          <h1>Welcome to your<br/>professional community</h1>
+          <input type="text" name="input" placeholder="Email or phone number"/>
+          <input type="password" name="pass" placeholder="Password"/>
+          <a href="#">Forgot password?</a>
+          <input class="mainbtn" type="button" value="Sign in"/>
+        </div>
+        <div class="right-section">
+          
+        </div>
+      </section> */}
+   </>
+  );
+}
+
+export default App;
