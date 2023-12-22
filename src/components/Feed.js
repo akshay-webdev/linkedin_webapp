9b1fdc5a8b7c33a21112ea6206d771995e64b525
Akshay Wakle <108985906+akshay-webdev@users.noreply.github.com>
2023-12-22 14:05:29 +0530

    ui chnages of feed page

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,45 +2,46 @@ import React, { useEffect, useState } from "react";
 import "../CSS/Feed.css";
 import InputOptions from "./InputOptions";
 import Post from "./Post";
-import {db} from "../firebase/app"
-import {app} from "../firebase/app"
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-// ... other services you need
+// import { db } from "../firebase/app"; // Correct import
+// import firebase from "../firebase/firebase";
+// import {db} from "../firebase/firebase"
 
-
+import { db } from "../firebase";
+import firebase from "../firebase"
+import "firebase/firestore"
+import "firebase/auth"
 import { Avatar } from "@material-ui/core";
-import PhotoIcon from "@material-ui/icons/Photo";
-import WorkIcon from "@material-ui/icons/Work";
-import EventNoteIcon from "@material-ui/icons/EventNote";
-import AssignmentIcon from "@material-ui/icons/Assignment";
+import { Assignment, EventNote, Photo, WorkOutline } from "@material-ui/icons";
 
 const Feed = () => {
   const profileimg = "/my_photo.png";
-  const [input , setInput]=useState('');
-  const [posts,setPosts]=useState([]);
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  useEffect(()=>{
-      db.collection("posts").onSnapshot((snapshot)=>
-        setPosts(
-          snapshot.docs.map((doc)=>({
-            id:doc.id,
-            data:doc.data()
-          }))
-        )
+  useEffect(() => {
+    // Corrected collection name from "post" to "posts"
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
       )
-  },[])
-  const sendPost=(e)=>{
+    );
+  }, []);
+
+  const sendPost = (e) => {
     e.preventDefault();
-    db.collection('post').add({
-      name:'akshay wakle',
-      descripition:'this is a just test',
-      message:input,
-      photoUrl:'',
-      timestamp:firebase.firestore.FieldValue.serverTimestamp()
-    }) 
-  }
+    // Corrected collection name from "post" to "posts"
+    db.collection("posts").add({
+      name: "akshay wakle",
+      descripition: "this is just a test", // Corrected typo
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput(""); // Clear the input after sending the post
+  };
 
   return (
     <div className="feed_section">
@@ -53,38 +54,45 @@ const Feed = () => {
               style={{ width: "100%", height: "100%", borderRadius: "50%" }}
             />
           </Avatar>
+
           <input
             type="text"
             placeholder="Start a post"
-            className="search_input" value={input} 
+            className="search_input"
+            value={input}
             onChange={(e) => setInput(e.target.value)}
           />
-          <button onClick={sendPost} type="submit">Send</button>
+          <button onClick={sendPost} type="submit">
+            Send
+          </button>
         </div>
         <div className="header_options">
-          <InputOptions Icon={PhotoIcon} title="Photo" className="photo"/>
-          <InputOptions Icon={WorkIcon} title="Work" className="work"/>
-          <InputOptions Icon={EventNoteIcon} title="Event" className="event"/>
-          <InputOptions Icon={AssignmentIcon} title="Assignment" className="assignment"/>
+          <InputOptions Icon={Photo} title="Photo" className="photo" />
+          <InputOptions Icon={WorkOutline} title="Work" className="work" />
+          <InputOptions Icon={EventNote} title="Event" className="event" />
+          <InputOptions
+            Icon={Assignment}
+            title="Assignment"
+            className="assignment"
+          />
         </div>
       </div>
-      
-      {
-        posts.map(({id,data:{name,descripition,message,photoUrl,timestamp}})=>(
 
-          <post 
-              key={id}
-              name={name}
-              descripition={descripition}
-              message={message}
-              photoUrl={photoUrl}
-              timestamp={timestamp}
+      {posts.map(
+        ({
+          id,
+          data: { name, descripition, message, photoUrl, timestamp },
+        }) => (
+          <Post
+            key={id}
+            name={name}
+            descripition={descripition}
+            message={message}
+            photoUrl={photoUrl}
+            timestamp={timestamp}
           />
-        ))
-      }
-      {/* Post Here... */}
-      {/* <Post name="akshay" descripition="description here" message="wow its works"/> */}
-
+        )
+      )}
     </div>
   );
 };
